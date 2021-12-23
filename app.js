ea51c6fd3d754d9d0f1aa000c2e0ccdc4ae13905
IchBinTiMo <lintimo1999@gmail.com>
2021-12-23 17:26:24 +0800

    init

---
+++ app.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const {machine} = require("./machine");
+const linebot = require("linebot");
+
+
+
+let bot = linebot(
+  {
+    channelId: process.env.CHANNEL_ID,
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+  });
+
+
+bot.on('message', function(event)
+{
+  let input;
+  switch(event.message.text){
+    case "go to state 1":
+      input = "state1";
+      break;
+    case "go to state 2":
+      input = "state2";
+      break
+    case "go back":
+      input = "user";
+      break;
+  }
+  let respond = machine["state"][machine.current]
+
+  if(respond){
+    respond = respond.apply(machine)
+  }
+  else{
+    respond = "Not Entering any State";
+  }
+
+  event.reply(respond).then(function()
+  {
+    console.log("Respond Successfully!");
+  }).catch(function()
+  {
+    console.log("Respond Failed!");
+  });
+  
+});
+
+bot.listen("/linewebhook", 3000, function(response)
+{
+  console.log(response);
+  console.log("listening at 3000...");
+});
+
+// console.log(machine.state.user.outFunction.state1);
+// let respond = machine["state"]["user"]["state2"];
+// if(respond){
+//   respond = respond.apply(machine);
+// }
+// else{
+//   console.log("Not Entering any State");
+// }
+// console.log(machine.current, respond);
---
+++ machine.js
@@ -0,0 +1,43 @@
+const machine = {
+  current: "user",
+  state: {
+    user: {
+      state1: function(){
+        return this.goToState_1();
+      }, 
+      state2: function() {
+        return this.goToState_2();
+      }
+    },
+    state1: {
+      user: function() {
+        return this.goBack();
+      }
+    }
+  },
+  goBack(){
+    let message = "Trigger user\nSelect state1 or state2";
+    this.setState("user");
+    return message;
+    
+  },
+  goToState_1(){
+    let message = "Trigger state1";
+    this.changeState("state1");
+    return message;
+  },
+
+  goToState_2(){
+    let message = "Trigger state2";
+    this.changeState("state2");
+    return message;
+  },
+
+  changeState(newState){
+    this.current = newState;
+  }
+}
+
+
+
+module.exports = {machine};

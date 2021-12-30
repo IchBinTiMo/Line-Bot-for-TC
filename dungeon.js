34da995f5cfef0de59d6631e53bddce53d87195f
IchBinTiMo <lintimo1999@gmail.com>
2021-12-31 02:01:08 +0800

    modify event pool and fix foward function

--- dungeon.js
+++ dungeon.js
@@ -1,35 +1,39 @@
-const enemySpawner = require('./enemy').enemySpawner;
+const enemySpawner = require('./enemy');
 class dungeon
 {
   constructor(){
-    this.event = ["enemy", "enemy", "enemy", "enemy", "enemy", "enemy", "enemy", "treasure", "nothing", "nothing"];
+    this.event = new Array(10);
+    this.event.fill(0);
+    this.event = this.event.map((x, i) => (i < 6) ? 1 : ( i < 7 ) ? 2 : 3)
     this.level = 1;
     this.fighting = 0;
     this.type = "nothing";
   }
   foward(){
-    let t = Math.random(this.event.length);
-    console.log(t);
-    return newStage(this.event[t]);
+    let t = Math.floor(Math.random() * this.event.length);
+    return this.nextStage(this.event[t]);
   }
   nextStage(type){
-    if(type == "nothing"){
+    if(type == 3){
+      this.type = "nothing"
       return {
-        type: type,
+        type: this.type,
         msg: "There is nothing here..."}
     }
-    else if(type == "enemy"){
+    else if(type == 1){
       this.fighting = 1;
       let e = new enemySpawner();
+      this.type = "enemy"
       return {
-        type: type, 
+        type: this.type, 
         msg: "An enemy blocks your way",
         enemy: e
       }
     }
-    else if(type == "treasure"){
+    else if(type == 2){
+      this.type = "treasure"
       return{
-        type: type,
+        type: this.type,
         msg: "You see something..."
       }
     }

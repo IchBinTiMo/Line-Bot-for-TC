b0af7b6cb82dbfaae3422c0b10a033326e324c76
IchBinTiMo <lintimo1999@gmail.com>
2022-01-02 13:01:03 +0800

    add enemy.currentHp, change the power of basic from 1/3 to 1.5, implement enemy.attack and enemy,underAttack and delete console.log for debugging

--- enemy.js
+++ enemy.js
@@ -1,13 +1,23 @@
 class enemy
 {
   constructor(level, hpSeed, atkSeed, defSeed, coinSeed, basic){
-    this.hp = Math.floor((100 + 2 * level * Math.sqrt(level) * (1 + hpSeed)) * Math.pow(basic, 1/3) * (1 + hpSeed));
+    this.maxHp = Math.floor((50 + 2 * level * Math.sqrt(level) * (1 + hpSeed)) * Math.pow(basic, 1.5) * (1 + hpSeed));
+    this.currentHp = this.maxHp;
     this.atk = Math.floor((10 + 2 * level * Math.sqrt(level) * (1 + atkSeed)) * Math.pow(basic, 1/3) * (1 + atkSeed));
     this.def = Math.floor((10 + 2 * level * Math.sqrt(level) * (1 + defSeed)) * Math.pow(basic, 1/3) * (1 + defSeed));
     this.coin = Math.floor(2 * level * Math.sqrt(level) * Math.pow(basic, 1.5) * (1 + coinSeed));
   }
   attack(){
-
+    let interval = 0.2
+    let random = Math.random() * 2 * interval - interval;
+    return Math.floor(this.atk * (1 + random));
+  }
+  underAttack(atk){
+    let interval = 0.2
+    let random = Math.random() * 2 * interval - interval;
+    let dmg = Math.floor(atk / this.def / 0.6 * Math.pow(atk, 2/3) * (1 + random));
+    this.currentHp = this.currentHp - dmg;
+    return dmg;
   }
 }
 
@@ -29,7 +39,7 @@ class enemySpawner
     let as = a * 2 * this.interval - this.interval;
     let ds = d * 2 * this.interval - this.interval;
     let cs = (h + a + d) * this.interval;
-    console.log(level, this.pool[i], h, hs, a, as, d, ds);
+    // console.log(level, this.pool[i], h, hs, a, as, d, ds);
     let e = new enemy(level, hs, as, ds, cs, this.pool[i]);
     return e;
 

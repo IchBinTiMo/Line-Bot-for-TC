bde79709a69cf6eaa8f00c921646ec89f107db93
IchBinTiMo <lintimo1999@gmail.com>
2022-01-02 12:59:02 +0800

    delete dungeon.type and simplify the dungeon.nextStage function

--- dungeon.js
+++ dungeon.js
@@ -7,35 +7,21 @@ class dungeon
     this.event = this.event.map((x, i) => (i < 6) ? 1 : ( i < 7 ) ? 2 : 3)
     this.level = 1;
     this.fighting = 0;
-    this.type = "nothing";
   }
-  foward(){
+  forward(){
     let t = Math.floor(Math.random() * this.event.length);
     return this.nextStage(this.event[t]);
   }
   nextStage(type){
     if(type == 3){
-      this.type = "nothing"
-      return {
-        type: this.type,
-        msg: "There is nothing here..."}
+      return "nothing";
     }
     else if(type == 1){
-      this.fighting = 1;
       let e = new enemySpawner();
-      this.type = "enemy"
-      return {
-        type: this.type, 
-        msg: "An enemy blocks your way",
-        enemy: e
-      }
+      return "enemy";
     }
     else if(type == 2){
-      this.type = "treasure"
-      return{
-        type: this.type,
-        msg: "You see something..."
-      }
+      return "treasure";
     }
    
   }

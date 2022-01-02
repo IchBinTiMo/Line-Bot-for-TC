6a624c8a5533c2798d58efc7ea3337de242cf443
IchBinTiMo <lintimo1999@gmail.com>
2022-01-02 12:54:58 +0800

    create potion.js and implement heal potion S

---
+++ potion.js
@@ -0,0 +1,15 @@
+const potions = () => {
+  return [
+    {
+      id: 0,
+      name: "heal potion s",
+      effect: "heal",
+      price: 10,
+      number: "10",
+      unit: "%",
+      target: "player"
+    }
+  ]
+}
+
+module.exports = potions;

4d2250132d199a7b5b59e628ee9d9248a3d82d69
IchBinTiMo <lintimo1999@gmail.com>
2022-01-02 12:57:40 +0800

    add player.item, player.name player.underAttack, and player.attack, and increase basic hp from 50 to 100

--- player.js
+++ player.js
@@ -1,11 +1,30 @@
 class player
 {
   constructor(maxHp, currentHp, atk, def, coin){
-    this.maxHp = maxHp;
-    this.currentHp = currentHp;
-    this.atk = atk;
-    this.def = def;
-    this.coin = coin;
+    this.maxHp = 100;
+    this.currentHp = 100;
+    this.atk = 15;
+    this.def = 15;
+    this.coin = 0;
+    this.name = "";
+    this.item = [];
+  }
+  attack(){
+    let interval = 0.2
+    let random = Math.random() * 2 * interval - interval;
+    return Math.floor(this.atk * (1 + random));
+  }
+  underAttack(atk){
+    let interval = 0.2
+    let random = Math.random() * 2 * interval - interval;
+    let dmg = Math.floor(atk / this.def / 0.6 * Math.pow(atk, 2/3) * (1 + random));
+    this.currentHp = this.currentHp - dmg;
+    return dmg;
+  }
+  state(){
+    return{
+
+    }
   }
 }
 
